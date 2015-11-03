fb782dd2934eed779b5bc14501d5356fd796b949
Chuanxun <chuanxun@chuanxuntekimac-mini.local>
2015-11-03 16:40:36 +0800

    Initial Commit

---
+++ AFN_Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AFN_Demo
+//
+//  Created by Chuanxun on 15/11/3.
+//  Copyright © 2015年 eLane. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AFN_Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AFN_Demo
+//
+//  Created by Chuanxun on 15/11/3.
+//  Copyright © 2015年 eLane. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
